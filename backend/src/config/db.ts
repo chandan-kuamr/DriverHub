4695f9f49fe37f8f9497eb711903153c0ebdb595
chandan-kuamr <chandnkumarbg77@gmail.com>
2026-09-19 14:35:15 +0530

    url is changed for deloying in atlas

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 const db = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/driverhub");
-
+    await mongoose.connect("mongodb+srv://chandnkumarbg77_db_user:4GQEYJRj7S8OlF7s@cluster1.7xcuxt5.mongodb.net/?appName=Cluster1");
+    
+  
     console.log("Database connection is successful");
   } catch (error) {
     console.log("Database connection failed", error);
